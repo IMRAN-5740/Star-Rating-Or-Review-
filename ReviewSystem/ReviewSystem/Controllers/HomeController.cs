fc8e7b4271932fa3c10d6712a47bd915bcc012a6
imran <imranbsmrstucse@gmail.com>
2023-02-09 09:20:53 +0600

    ReviewSystemAdded

--- ReviewSystem/ReviewSystem/Controllers/HomeController.cs
+++ ReviewSystem/ReviewSystem/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ReviewSystem.Models;
+using ReviewSystem.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +10,58 @@ namespace ReviewSystem.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _reviewSystemEntities;
+        public HomeController()
+        {
+            _reviewSystemEntities =new ApplicationDbContext();
+        }
+        // GET: Home
         public ActionResult Index()
         {
-            return View();
+            List<ArticleViewModel> listOfArticle = new List<ArticleViewModel>();
+            listOfArticle = (from article in _reviewSystemEntities.Articles
+                             select new ArticleViewModel()
+                             {
+                                 ArticleId = article.ArticleId,
+                                 ArticleName=article.ArticleName,
+                                 ArticleDescription=article.ArticleDescription
+                             }).ToList();
+            return View(listOfArticle);
         }
 
-        public ActionResult About()
+        public ActionResult ShowComment(int articleId)
         {
-            ViewBag.Message = "Your application description page.";
+            List<CommentViewModel> listOfComments = new List<CommentViewModel>();
+            listOfComments = (from comment in _reviewSystemEntities.Comments
+                              where comment.ArticleId == articleId
+                              select new CommentViewModel()
+                              {
+                                  ArticleId = comment.ArticleId,
+                                  CommentId = comment.CommentId,
+                                  CommentDescription = comment.CommentDescription,
+                                  Rating = comment.Rating,
+                                  CommentedOn = comment.CommentedOn
 
-            return View();
+                              }).ToList();
+            
+            ViewBag.ArticleId = articleId;
+            return View(listOfComments);
         }
-
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult AddComment(int articleId, int rating, string articleComment)
         {
-            ViewBag.Message = "Your contact page.";
+            Comment listComment = new Comment();
+            listComment.ArticleId = articleId;
+            listComment.Rating = rating;
+            listComment.CommentDescription = articleComment;
+            listComment.CommentedOn = DateTime.Now;
+            listComment.Rating = rating;
+            listComment.GuestId = Guid.NewGuid();
+            _reviewSystemEntities.Comments.Add(listComment);
+            _reviewSystemEntities.SaveChanges();
 
-            return View();
+            return RedirectToAction(nameof(Index));
         }
+
     }
 }
